91664ead1a7244a531517e5e621480c92890358d
Eddie Freeman <eddie.freeman@gmail.com>
2015-12-21 15:36:17 -0800

    added view skeletons, client manager skeletons, model skeletons

---
+++ GoodFeels/Clients/MessageClient.swift
@@ -0,0 +1,13 @@
+//
+//  MessageClient.swift
+//  GoodFeels
+//
+//  Created by Edward Freeman on 12/21/15.
+//  Copyright © 2015 NinjaSudo LLC. All rights reserved.
+//
+
+import Foundation
+
+class MessageClient: NSObject {
+
+}
---
+++ GoodFeels/Models/Contact.swift
@@ -0,0 +1,13 @@
+//
+//  Contact.swift
+//  GoodFeels
+//
+//  Created by Edward Freeman on 12/21/15.
+//  Copyright © 2015 NinjaSudo LLC. All rights reserved.
+//
+
+import Foundation
+
+class Contact: NSObject {
+
+}
