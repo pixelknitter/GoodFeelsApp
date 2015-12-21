bb033b0574b68ec6be7fe0da68a27e22a6767cb5
Eddie Freeman <eddie.freeman@gmail.com>
2015-12-21 14:52:06 -0800

    added git ignore

---
+++ GoodFeels/GoodFeels-Bridging-Header.h
@@ -0,0 +1,6 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <AppPulsemobile/HPAppPulse.h>
+//#import <AppPulsemobile/HPUserMonitoringSDK.h>
---
+++ GoodFeels/ViewControllers/MessagesViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MessagesViewController.swift
+//  GoodFeels
+//
+//  Created by Edward Freeman on 12/21/15.
+//  Copyright © 2015 NinjaSudo LLC. All rights reserved.
+//
+
+import Cocoa
+
+class MessagesViewController: UIViewController {
+
+}
